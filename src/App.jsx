50d981e28d36abfe957c3e51afd748b9f4c7d41e
“Rahul <dhakarahul16@gmail.com>
2024-06-27 22:47:02 +0530

    toggle button update

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import { useSelector } from 'react-redux';
 
 
 function App() {
+  document.querySelector('html').classList.remove('dark');
+
     const darkMode = useSelector((state)=> state.darkMode.darkMode);
-    console.log('new valueof dmode - ', darkMode);
+    // console.log('new valueof dmode - ', darkMode);
+    if(darkMode){
+      document.querySelector('html').classList.add('dark');
+    }
+   
 
   return (
     <>
-      <div  className={`${darkMode ? "dark" : ""}`}><h1 className='text-xl dark:bg-pink-400'>Hello redux basic project</h1>
+      <div><h1 className='text-xl dark:bg-pink-400'>Hello redux basic project</h1>
       <Counter/>
       <DMode/>
       </div>
--- src/features/darkMode/DMode.jsx
+++ src/features/darkMode/DMode.jsx
@@ -9,7 +9,8 @@ const DMode = () => {
     console.log('dmode is = ', darkMode)
   return (
     <div className='p-2 m-2'>
-        <button className='p-2 m-2' onClick={()=> dispatch(toggleDarkMode())} >Toggle dark mode</button>
+
+        <button className='p-2 m-2 border dark:bg-slate-700 dark:text-white' onClick={()=> dispatch(toggleDarkMode())} >Toggle dark mode</button>
     </div>
   )
 }
